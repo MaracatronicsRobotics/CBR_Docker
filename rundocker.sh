5f869d7b8d4b7e9eaf52c9e539e4041dee725798
Maracatronics <37337748+maracatronics@users.noreply.github.com>
2020-11-10 13:01:43 -0300

    Update rundocker.sh

--- rundocker.sh
+++ rundocker.sh
@@ -9,23 +9,15 @@ DOCKER_IMAGE=$DEFAULT_DOCKER_IMAGE
 
 # Executando o docker
 docker run  -it \
-            --user=$(id -u) \
-            --env="DISPLAY" \
+            --env="DISPLAY=:0" \
             --env="QT_X11_NO_MITSHM=1" \
             --name=$CONTAINER_NAME \
-            --memory=1024g \
-            --oom-kill-disable \
-            --ipc="host" \
             --volume="/dev:/dev" \
-            --privileged \
             --net=host \
+            --privileged \
             --workdir="${CONTAINER_WORK_DIR}" \
             --volume="${WORK_DIR}:${CONTAINER_WORK_DIR}" \
-            --volume="/etc/group:/etc/group:ro" \
-            --volume="/etc/passwd:/etc/passwd:ro" \
-            --volume="/etc/shadow:/etc/shadow:ro" \
-            --volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
-            --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+            -v $HOME/.Xauthority:/root/.Xauthority \
             $DOCKER_IMAGE
 
 docker container rm $CONTAINER_NAME -f
